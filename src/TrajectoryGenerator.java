91c79d148c28138f8caf1c7a579051a17a4ef49c
Rohan Viswanathan <rohanvis16@gmail.com>
2018-01-15 00:48:58 -0800

    Fixed cruise vel (adjusted velocity) calculations to generate graph correctly without a break.

--- src/TrajectoryGenerator.java
+++ src/TrajectoryGenerator.java
@@ -13,15 +13,12 @@ public class TrajectoryGenerator {
     }
 
     public Trajectory generateTrajectory(double startVel, double endVel, double distance){
-        cruiseVel = Math.min(Math.sqrt(Math.pow(startVel, 2) + 2*acc*(distance/2)), maxVel);
+        cruiseVel = Math.min(Math.sqrt((distance * acc) + ((Math.pow(startVel, 2) + Math.pow(endVel, 2))/2)), maxVel);
         double accelTime = (cruiseVel - startVel)/acc;
         double decelTime = (cruiseVel - endVel)/acc;
         accelDistance = startVel * accelTime + (0.5 * acc * Math.pow(accelTime, 2));
         decelDistance = cruiseVel * decelTime - (0.5 * acc * Math.pow(decelTime, 2));
         cruiseDistance = distance - accelDistance - decelDistance;
-        if (cruiseDistance < 0){
-            return new Trajectory(0);
-        }
         double cruiseTime = cruiseDistance/ cruiseVel;
         double totalTime = accelTime + decelTime + cruiseTime;
         int size = (int)(totalTime/dt);
@@ -41,6 +38,7 @@ public class TrajectoryGenerator {
                 currPos = accelDistance + (cruiseVel * (currTime - accelTime));
                 currVel = cruiseVel;
                 currAccel = 0;
+
             }
 
             else {
@@ -62,7 +60,7 @@ public class TrajectoryGenerator {
 
     public static void main (String [] args){
         TrajectoryGenerator trajectoryGenerator = new TrajectoryGenerator(12, 12, 0.005);
-        System.out.println(trajectoryGenerator.generateTrajectory(4, 4, 1));
+        System.out.println(trajectoryGenerator.generateTrajectory(10, 0, 5));
         System.out.println("Accel Dist " + trajectoryGenerator.accelDistance);
         System.out.println("Decel Dist " + trajectoryGenerator.decelDistance);
         System.out.println("Cruise Dist " + trajectoryGenerator.cruiseDistance);
